3bf6dcb8a03a6c8e927db8bec6a199ca9e92f291
Mithun Biswas <bhoot.biswas@gmail.com>
2018-06-06 19:46:30 +0600

    Remove options page

--- includes/admin-options.php
+++ includes/admin-options.php
@@ -32,8 +32,6 @@ class WPOAuth_Admin {
 		require_once( dirname( __FILE__ ) . "/admin/pages/add-new-client.php" );
 		require_once( dirname( __FILE__ ) . "/admin/pages/manage-clients.php" );
 		require_once( dirname( __FILE__ ) . "/admin/pages/edit-client.php" );
-
-		require_once( dirname( __FILE__ ) . "/admin/page-server-options.php" );
 	}
 
 	/**
@@ -42,7 +40,6 @@ class WPOAuth_Admin {
 	public function add_page() {
 		add_menu_page( 'OAuth Server', 'OAuth Server', 'manage_options', 'wo_manage_clients', "wo_admin_manage_clients_page", 'dashicons-groups' );
 		add_submenu_page( 'wo_manage_clients', 'Clients', __( 'Clients', 'wp-oauth' ), 'manage_options', 'wo_manage_clients', 'wo_admin_manage_clients_page' );
-		add_submenu_page( 'wo_manage_clients', 'Settings', __( 'Settings', 'wp-oauth' ), 'manage_options', 'wo_settings', "wo_server_options_page" );
 		add_submenu_page( null, 'Add Client', 'Add Client', 'manage_options', 'wo_add_client', 'wo_add_client_page' );
 		add_submenu_page( null, 'Edit Client', 'Edit Clients', 'manage_options', 'wo_edit_client', 'wo_admin_edit_client_page' );
 	}
