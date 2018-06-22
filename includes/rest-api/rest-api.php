0abf1343ec14bcea5992492b67d41fdd911117f1
Rajendra Banker <bankerrajendra@upnrunn.com>
2018-06-22 16:23:37 +0530

    Added customer joined date

--- includes/rest-api/rest-api.php
+++ includes/rest-api/rest-api.php
@@ -1,27 +1,45 @@
 <?php
-add_action( 'rest_api_init', 'wh_user_meta_fields' );
-function wh_user_meta_fields() {
-    register_rest_field( 'user', 'last_login', array(
+add_action('rest_api_init', 'wh_user_meta_fields');
+function wh_user_meta_fields()
+{
+    register_rest_field(
+        'user',
+        'last_login',
+        array(
            'get_callback'    => 'wh_get_user_last_login',
            'schema'          => null,
         )
     );
 
-    register_rest_field( 'user', 'has_avatar', array(
+    register_rest_field(
+        'user',
+        'has_avatar',
+        array(
            'get_callback'    => 'wh_get_check_user_avatar',
+           'schema'          => null,
+        )
+    );
+
+    // user registered date formatted
+    register_rest_field(
+        'user',
+        'joined_date',
+        array(
+           'get_callback'    => 'wh_get_user_joined_date',
            'schema'          => null,
         )
     );
 }
 
-function wh_get_user_last_login( $user ) {
-    $session_tokens = get_user_meta( $user['id'], 'session_tokens', true );
+function wh_get_user_last_login($user)
+{
+    $session_tokens = get_user_meta($user['id'], 'session_tokens', true);
     $sessions = array();
 
-    if ( ! empty( $session_tokens ) ) {
+    if (! empty($session_tokens)) {
         foreach ($session_tokens as $key => $session) {
             $session['token'] = $key;
-            $session['login_diff'] = human_time_diff( $session['login'], current_time('timestamp') ) . ' ago';
+            $session['login_diff'] = human_time_diff($session['login'], current_time('timestamp')) . ' ago';
             $sessions[] = $session;
         }
     }
@@ -29,12 +47,21 @@ function wh_get_user_last_login( $user ) {
     return $sessions[0];
 }
 
-function wh_get_check_user_avatar( $user ) {
-    return wh_has_gravatar( $user['email'] );
+function wh_get_check_user_avatar($user)
+{
+    return wh_has_gravatar($user['email']);
 }
 
-function wh_has_gravatar( $email_address ) {
-	$url = 'http://www.gravatar.com/avatar/' . md5( strtolower( trim ( $email_address ) ) ) . '?d=404';
-	$headers = @get_headers( $url );
-	return preg_match( '|200|', $headers[0] ) ? true : false;
+function wh_has_gravatar($email_address)
+{
+    $url = 'http://www.gravatar.com/avatar/' . md5(strtolower(trim($email_address))) . '?d=404';
+    $headers = @get_headers($url);
+    return preg_match('|200|', $headers[0]) ? true : false;
+}
+
+function wh_get_user_joined_date($user)
+{
+    $udata = get_userdata($user['id']);
+    $registered = $udata->data->user_registered;
+    return date("M d, Y", strtotime($registered));
 }
