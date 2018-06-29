dbeedfac186aecd65f61b2708dac7c09eae943f7
Rajendra Banker <bankerrajendra@upnrunn.com>
2018-06-29 19:28:13 +0530

    Added separate end point for recent events instead of dashboard

--- includes/actions.php
+++ includes/actions.php
@@ -16,13 +16,14 @@
  *
  * @since 3.1.8
  */
-function wo_password_reset_action( $user, $new_pass ) {
-	global $wpdb;
-	$wpdb->delete( "{$wpdb->prefix}oauth_access_tokens", array( "user_id" => $user->ID ) );
-	$wpdb->delete( "{$wpdb->prefix}oauth_refresh_tokens", array( "user_id" => $user->ID ) );
+function wo_password_reset_action($user, $new_pass)
+{
+    global $wpdb;
+    $wpdb->delete("{$wpdb->prefix}oauth_access_tokens", array( "user_id" => $user->ID ));
+    $wpdb->delete("{$wpdb->prefix}oauth_refresh_tokens", array( "user_id" => $user->ID ));
 }
 
-add_action( 'password_reset', 'wo_password_reset_action', 10, 2 );
+add_action('password_reset', 'wo_password_reset_action', 10, 2);
 
 /**
  * [wo_profile_update_action description]
@@ -31,16 +32,17 @@ add_action( 'password_reset', 'wo_password_reset_action', 10, 2 );
  *
  * @return Void
  */
-function wo_profile_update_action( $user_id ) {
-	if ( ! isset( $_POST['pass1'] ) || '' == $_POST['pass1'] ) {
-		return;
-	}
-	global $wpdb;
-	$wpdb->delete( "{$wpdb->prefix}oauth_access_tokens", array( "user_id" => $user_id ) );
-	$wpdb->delete( "{$wpdb->prefix}oauth_refresh_tokens", array( "user_id" => $user_id ) );
+function wo_profile_update_action($user_id)
+{
+    if (! isset($_POST['pass1']) || '' == $_POST['pass1']) {
+        return;
+    }
+    global $wpdb;
+    $wpdb->delete("{$wpdb->prefix}oauth_access_tokens", array( "user_id" => $user_id ));
+    $wpdb->delete("{$wpdb->prefix}oauth_refresh_tokens", array( "user_id" => $user_id ));
 }
 
-add_action( 'profile_update', 'wo_profile_update_action' );
+add_action('profile_update', 'wo_profile_update_action');
 
 /**
  * Only allow 1 acces_token at a time
@@ -49,79 +51,81 @@ add_action( 'profile_update', 'wo_profile_update_action' );
  *
  * @return [type]          [description]
  */
-function wo_only_allow_one_access_token( $object ) {
-	if ( is_null( $object ) ) {
-		return;
-	}
+function wo_only_allow_one_access_token($object)
+{
+    if (is_null($object)) {
+        return;
+    }
 
-	// Define the user ID
-	$user_id = $object['user_id'];
+    // Define the user ID
+    $user_id = $object['user_id'];
 
-	// Remove all other access tokens and refresh tokens from the system
-	global $wpdb;
-	$wpdb->delete( "{$wpdb->prefix}oauth_access_tokens", array( "user_id" => $user_id ) );
-	$wpdb->delete( "{$wpdb->prefix}oauth_refresh_tokens", array( "user_id" => $user_id ) );
+    // Remove all other access tokens and refresh tokens from the system
+    global $wpdb;
+    $wpdb->delete("{$wpdb->prefix}oauth_access_tokens", array( "user_id" => $user_id ));
+    $wpdb->delete("{$wpdb->prefix}oauth_refresh_tokens", array( "user_id" => $user_id ));
 
-	return;
+    return;
 }
 
 /**
  * Restrict users to only have a single access token
  * @since 3.2.7
  */
-$wo_restrict_single_access_token = apply_filters( 'wo_restrict_single_access_token', false );
-if ( $wo_restrict_single_access_token ) {
-	add_action( 'wo_set_access_token', 'wo_only_allow_one_access_token' );
+$wo_restrict_single_access_token = apply_filters('wo_restrict_single_access_token', false);
+if ($wo_restrict_single_access_token) {
+    add_action('wo_set_access_token', 'wo_only_allow_one_access_token');
 }
 
 // validating requester
-add_filter( 'rest_pre_dispatch', 'validate_wpdrift_request' , 10, 3 );
-function validate_wpdrift_request( $result, $server, $request ) {
-	$route = $request->get_route();
-	if($route == "/wpdriftsupporter/v1/validate-n-save-host") {
-		$host = $_SERVER['REMOTE_ADDR'];
-		if ( '167.99.167.87' !== $host ) {
-			// Referer is set to something that we don't allow.
-			return 'Invalid Host';
-		} else {
-			// setup the credentials and
-			// Go Ahead!
-			$params = $request->get_params('POST');
-			$client_id     = wo_gen_key();
-			$client_secret = wo_gen_key();
-			// Add host
-			$grant_types = array(
-				'authorization_code',
-				'implicit',
-				'password',
-				'client_credentials',
-				'refresh_token'
-			);
-			$client_data = array(
-				'post_title'     => $params['store_name'],
-				'post_status'    => 'publish',
-				'post_author'    => '1',
-				'post_type'      => 'wo_client',
-				'comment_status' => 'closed',
-				'meta_input'     => array(
-					'client_id'     => $client_id,
-					'client_secret' => $client_secret,
-					'grant_types'   => $grant_types,
-					'redirect_uri'  => $params['return_url'],
-					'user_id'       => '-1'
-				)
-			);
+add_filter('rest_pre_dispatch', 'validate_wpdrift_request', 10, 3);
+function validate_wpdrift_request($result, $server, $request)
+{
+    $route = $request->get_route();
+    if ($route == "/wpdriftsupporter/v1/validate-n-save-host") {
+        $host = $_SERVER['REMOTE_ADDR'];
+        if ('167.99.167.87' !== $host) {
+            // Referer is set to something that we don't allow.
+            return 'Invalid Host';
+        } else {
+            // setup the credentials and
+            // Go Ahead!
+            $params = $request->get_params('POST');
+            $client_id     = wo_gen_key();
+            $client_secret = wo_gen_key();
+            // Add host
+            $grant_types = array(
+                'authorization_code',
+                'implicit',
+                'password',
+                'client_credentials',
+                'refresh_token'
+            );
+            $client_data = array(
+                'post_title'     => $params['store_name'],
+                'post_status'    => 'publish',
+                'post_author'    => '1',
+                'post_type'      => 'wo_client',
+                'comment_status' => 'closed',
+                'meta_input'     => array(
+                    'client_id'     => $client_id,
+                    'client_secret' => $client_secret,
+                    'grant_types'   => $grant_types,
+                    'redirect_uri'  => $params['return_url'],
+                    'user_id'       => '-1'
+                )
+            );
 
-			wp_insert_post( $client_data );
+            wp_insert_post($client_data);
 
-			$client_data['store_id'] = $params['sid'];
+            $client_data['store_id'] = $params['sid'];
 
-			return $client_data;
-		}
-		return $result;
-	}
-	// Otherwise we are good - return original result and let WordPress handle as usual.
-	return $result;
+            return $client_data;
+        }
+        return $result;
+    }
+    // Otherwise we are good - return original result and let WordPress handle as usual.
+    return $result;
 }
 
 // Debugging part
@@ -140,40 +144,47 @@ if (!function_exists('_custlog')) {
     }
 }
 
-add_action( 'rest_api_init', function () {
-	register_rest_route( 'wpdriftsupporter/v1', '/validate-n-save-host/', array(
-		'methods' => 'POST',
-		'callback' => 'call_for_validating_client_hosts',
-	) );
+add_action('rest_api_init', function () {
+    register_rest_route('wpdriftsupporter/v1', '/validate-n-save-host/', array(
+        'methods' => 'POST',
+        'callback' => 'call_for_validating_client_hosts',
+    ));
 
-	register_rest_route( 'wpdriftsupporter/v1', '/validate-token/', array(
-		'methods' => 'POST',
-		'callback' => 'wpdrit_validate_token',
-	) );
+    register_rest_route('wpdriftsupporter/v1', '/validate-token/', array(
+        'methods' => 'POST',
+        'callback' => 'wpdrit_validate_token',
+    ));
 
-	require_once( dirname( WPDRIFT_HELPER_FILE ) . '/includes/rest-api/class-wpdrift-dashboard-endpoint.php' );
-	$dashboard_controller = new WD_Dashboard_Endpoint();
-	$dashboard_controller->register_routes();
-} );
+    require_once(dirname(WPDRIFT_HELPER_FILE) . '/includes/rest-api/class-wpdrift-dashboard-endpoint.php');
+    $dashboard_controller = new WD_Dashboard_Endpoint();
+    $dashboard_controller->register_routes();
 
-function call_for_validating_client_hosts() {
+    // Register new recent events end points
+    require_once(dirname(WPDRIFT_HELPER_FILE) . '/includes/rest-api/class-wpdrift-recentevents-endpoint.php');
+    $recent_event_controller = new WD_RecentEvents_Endpoint();
+    $recent_event_controller->register_routes();
+});
 
+function call_for_validating_client_hosts()
+{
 }
 
-function wpdrit_validate_token( $data ) {
-	global $wpdb;
-	$host = $_SERVER['REMOTE_ADDR'];
-	$access_token = $data['access_token'];
+function wpdrit_validate_token($data)
+{
+    global $wpdb;
+    $host = $_SERVER['REMOTE_ADDR'];
+    $access_token = $data['access_token'];
 
-	if ( '167.99.167.87' !== $host ) {
-		return 'Invalid Host';
-	} else {
-		return $wpdb->get_row( "SELECT * FROM {$wpdb->prefix}oauth_access_tokens WHERE access_token = '{$access_token}'", OBJECT );
-	}
+    if ('167.99.167.87' !== $host) {
+        return 'Invalid Host';
+    } else {
+        return $wpdb->get_row("SELECT * FROM {$wpdb->prefix}oauth_access_tokens WHERE access_token = '{$access_token}'", OBJECT);
+    }
 }
 
-function wh_save_login_activity( $user_login, $user ) {
-	$session_tokens = get_user_meta($user->ID, 'session_tokens', true);
+function wh_save_login_activity($user_login, $user)
+{
+    $session_tokens = get_user_meta($user->ID, 'session_tokens', true);
     $sessions = array();
 
     if (! empty($session_tokens)) {
@@ -183,15 +194,15 @@ function wh_save_login_activity( $user_login, $user ) {
         }
     }
 
-	update_user_meta($user->ID, 'last_login', $session);
+    update_user_meta($user->ID, 'last_login', $session);
 
-	$ip_data = wh_get_user_ip_data( $session['ip'] );
-	$ip_data = json_decode($ip_data, true);
-	if ( ! empty( $ip_data )  && ( 'success' == $ip_data['status'] ) ) {
-		update_user_meta($user->ID, 'ip_data', $ip_data );
+    $ip_data = wh_get_user_ip_data($session['ip']);
+    $ip_data = json_decode($ip_data, true);
+    if (! empty($ip_data)  && ('success' == $ip_data['status'])) {
+        update_user_meta($user->ID, 'ip_data', $ip_data);
         foreach ($ip_data as $key => $value) {
-            update_user_meta($user->ID, 'ip_' . $key, $value );
+            update_user_meta($user->ID, 'ip_' . $key, $value);
         }
-	}
+    }
 }
 add_action('wp_login', 'wh_save_login_activity', 999, 2);
---
+++ includes/rest-api/class-wpdrift-recentevents-endpoint.php
@@ -0,0 +1,198 @@
+<?php
+/**
+ * WD_RecentEvents_Endpoint class
+ */
+
+defined('ABSPATH') || exit;
+
+/**
+ * RecentEvents endpoints.
+ *
+ * @since 1.0.0
+ */
+class WD_RecentEvents_Endpoint extends WP_REST_Controller
+{
+    /**
+     * Constructor.
+     *
+     * @since 1.0.0
+     */
+    public function __construct()
+    {
+        $this->namespace = 'wpdriftsupporter/v1';
+        $this->rest_base = 'recentevents';
+    }
+
+    /**
+     * Register the component routes.
+     *
+     * @since 1.0.0
+     */
+    public function register_routes()
+    {
+        register_rest_route($this->namespace, '/' . $this->rest_base, array(
+            array(
+                'methods'             => WP_REST_Server::READABLE,
+                'callback'            => array( $this, 'get_items' ),
+                'permission_callback' => array( $this, 'get_items_permissions_check' ),
+                'args'                => array(
+
+                ),
+            )
+        ));
+    }
+
+    /**
+     * Get a collection of items
+     *
+     * @param WP_REST_Request $request Full data about the request.
+     * @return WP_Error|WP_REST_Response
+     */
+    public function get_items($request)
+    {
+        $parameters = $request->get_params();
+        $items = array();
+        $items['recent_events'] = $this->retrieve_recent_events($parameters);
+        $data = array();
+        foreach ($items as $key => $item) {
+            $itemdata = $this->prepare_item_for_response($item, $request);
+            $data[$key] = $this->prepare_response_for_collection($itemdata);
+        }
+
+        return rest_ensure_response($data);
+    }
+
+    /**
+     * Prepare the item for the REST response
+     *
+     * @param mixed $item WordPress representation of the item.
+     * @param WP_REST_Request $request Request object.
+     * @return mixed
+     */
+    public function prepare_item_for_response($item, $request)
+    {
+        return $item;
+    }
+
+    /**
+     * Check if a given request has access to get items
+     *
+     * @param WP_REST_Request $request Full data about the request.
+     * @return WP_Error|bool
+     */
+    public function get_items_permissions_check($request)
+    {
+        return current_user_can('list_users');
+    }
+
+
+    /**
+    * Retrieve Recent Sigup Users
+    *
+    * @since 1.0.0
+    */
+    public function retrieve_recent_events($parameters)
+    {
+        $type = (isset($parameters['type']) && trim($parameters['type']) != "") ? trim($parameters['type']) : "all";
+        global $wpdb;
+        $last_ten_uids = $wpdb->get_results("SELECT ID FROM $wpdb->users ORDER BY ID DESC LIMIT 10");
+        $users_ids = array();
+        foreach ($last_ten_uids as $last_ten_uid) {
+            $users_ids[] = $last_ten_uid->ID;
+        }
+        $args = array(
+          'include' => $users_ids
+        );
+
+        $users = get_users($args);
+        if ($type == "comment") {
+            $users = array();
+        }
+        // Get recent 10 comments
+        $args_cmts = array(
+            'orderby' => array('comment_date'),
+            'order' => 'DESC',
+            'number' => 10
+        );
+
+        $comments = get_comments($args_cmts);
+        if ($type == "user") {
+            $comments = array();
+        }
+
+        $recent_events_raw = array_merge($comments, $users);
+        $i = 0;
+        $recent_events = array();
+        foreach ($recent_events_raw as $recent_event) {
+            if ($recent_event != null) {
+                if ($recent_event->ID != "") {
+                    $key_for_sort = strtotime($recent_event->data->user_registered);
+                    // User Sign Up
+                    $recent_events[$key_for_sort]['event_type'] = 'signup';
+                    $recent_events[$key_for_sort]['event_id'] = $recent_event->ID;
+                    $recent_events[$key_for_sort]['user_display_name'] = $this->get_display_name_by_id($recent_event->ID);
+                    $recent_events[$key_for_sort]['event_date'] = $this->get_event_date($recent_event->data->user_registered);
+                    $recent_events[$key_for_sort]['user_avatar'] = $this->get_user_avatar_by_email($recent_event->data->user_email);
+                    $recent_events[$key_for_sort]['user_id'] = $recent_event->ID;
+                }
+                if ($recent_event->comment_ID != "") {
+                    // Comment
+                    $key_for_sort = strtotime($recent_event->comment_date);
+                    $recent_events[$key_for_sort]['event_type'] = 'comment';
+                    $recent_events[$key_for_sort]['event_id'] = $recent_event->comment_ID;
+                    $recent_events[$key_for_sort]['user_display_name'] = $recent_event->comment_author;
+                    $recent_events[$key_for_sort]['event_date'] = $this->get_event_date($recent_event->comment_date);
+                    $recent_events[$key_for_sort]['user_avatar'] = $this->get_user_avatar_by_email($recent_event->comment_author_email);
+                    $recent_events[$key_for_sort]['user_id'] = $recent_event->user_id;
+                }
+                $i++;
+            }
+        }
+        // Sort array by ksort
+        krsort($recent_events, 1);
+        $new_events = array();
+        $k = 0;
+        foreach ($recent_events as $recent_event) {
+            $new_events[$k]['event_type'] = $recent_event['event_type'];
+            $new_events[$k]['event_id'] = $recent_event['event_id'];
+            $new_events[$k]['user_display_name'] = $recent_event['user_display_name'];
+            $new_events[$k]['event_date'] = $recent_event['event_date'];
+            $new_events[$k]['user_avatar'] = $recent_event['user_avatar'];
+            $new_events[$k]['user_id'] = $recent_event['user_id'];
+            $k++;
+        }
+
+        return $new_events;
+    }
+
+    // get user display name by id
+    public function get_display_name_by_id($id)
+    {
+        $first_name = get_user_meta($id, 'first_name', true);
+        $last_name = get_user_meta($id, 'last_name', true);
+        $user_name = "";
+        if ($first_name == "" && $last_name == "") {
+            // get display name of user
+            $user_name = get_the_author_meta('display_name', $id);
+        } else {
+            $user_name = $first_name . " " . $last_name;
+        }
+        return $user_name;
+    }
+    // get user avatar by applied $email_address
+    public function get_user_avatar_by_email($email_address)
+    {
+        if (wh_has_gravatar($email_address)) {
+            $user_avatar = 'https://www.gravatar.com/avatar/' . md5(strtolower(trim($email_address))) . '?s=48&d=404';
+            ;
+        } else {
+            $user_avatar = "";
+        }
+        return $user_avatar;
+    }
+    // get event date by supplied date and format it and return
+    public function get_event_date($supplied_date)
+    {
+        return human_time_diff(strtotime($supplied_date), current_time('timestamp')) . " " . __('ago');
+    }
+}
