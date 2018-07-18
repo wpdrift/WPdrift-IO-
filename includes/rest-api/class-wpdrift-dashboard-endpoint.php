9f9e4a507020cd0c3938bd68e5dad5f9648b3b35
Mithun Biswas <bhoot.biswas@gmail.com>
2018-07-18 15:43:58 +0600

    Update dashboard endpoints

--- includes/rest-api/class-wpdrift-dashboard-endpoint.php
+++ includes/rest-api/class-wpdrift-dashboard-endpoint.php
@@ -3,150 +3,265 @@
  * WD_Dashboard_Endpoint class
  */
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
 /**
  * Dashboard endpoints.
  *
  * @since 1.0.0
  */
-class WD_Dashboard_Endpoint extends WP_REST_Controller
-{
-    /**
-     * Constructor.
-     *
-     * @since 1.0.0
-     */
-    public function __construct()
-    {
-        $this->namespace = 'wpdriftsupporter/v1';
-        $this->rest_base = 'dashboard';
-    }
+class WD_Dashboard_Endpoint extends WP_REST_Controller {
+	/**
+	 * Constructor.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct() {
+		$this->namespace = 'wpdriftsupporter/v1';
+		$this->rest_base = 'dashboard';
+	}
 
-    /**
-     * Register the component routes.
-     *
-     * @since 1.0.0
-     */
-    public function register_routes()
-    {
-        register_rest_route($this->namespace, '/' . $this->rest_base, array(
-            array(
-                'methods'             => WP_REST_Server::READABLE,
-                'callback'            => array( $this, 'get_items' ),
-                'permission_callback' => array( $this, 'get_items_permissions_check' ),
-                'args'                => array(
+	/**
+	 * Register the component routes.
+	 *
+	 * @since 1.0.0
+	 */
+	public function register_routes() {
+		register_rest_route($this->namespace, '/' . $this->rest_base, array(
+			array(
+				'methods'             => WP_REST_Server::READABLE,
+				'callback'            => array( $this, 'get_items' ),
+				'permission_callback' => array( $this, 'get_items_permissions_check' ),
+				'args'                => array(),
+			),
+		));
+	}
 
-                ),
-            )
-        ));
-    }
+	/**
+	 * Get a collection of items
+	 *
+	 * @param WP_REST_Request $request Full data about the request.
+	 * @return WP_Error|WP_REST_Response
+	 */
+	public function get_items( $request ) {
+		/**
+		 * [global description]
+		 * @var [type]
+		 */
+		global $wpdb;
+		$date_args = array();
 
-    /**
-     * Get a collection of items
-     *
-     * @param WP_REST_Request $request Full data about the request.
-     * @return WP_Error|WP_REST_Response
-     */
-    public function get_items($request)
-    {
-        $items = array();
-        $items['users'] = count_users();
-        $items['week_users'] = $this->get_weekly_users();
-        $items['posts'] = wp_count_posts();
-        $items['week_posts'] = $this->get_weekly_posts();
-        $items['pages'] = wp_count_posts('page');
-        $items['week_pages'] = $this->get_weekly_posts('page');
-        $items['comments'] = wp_count_comments();
-        $items['week_comments'] = $this->get_weekly_comments();
-        $data = array();
-        foreach ($items as $key => $item) {
-            $itemdata = $this->prepare_item_for_response($item, $request);
-            $data[$key] = $this->prepare_response_for_collection($itemdata);
-        }
+		/**
+		 * [if description]
+		 * @var [type]
+		 */
+		if ( isset( $request['after'] ) ) {
+			$date_args[0]['after'] = $request['after'];
+		}
 
-        return rest_ensure_response($data);
-    }
+		/**
+		 * [if description]
+		 * @var [type]
+		 */
+		if ( isset( $request['before'] ) ) {
+			$date_args[0]['before'] = $request['before'];
+		}
 
-    /**
-     * Prepare the item for the REST response
-     *
-     * @param mixed $item WordPress representation of the item.
-     * @param WP_REST_Request $request Request object.
-     * @return mixed
-     */
-    public function prepare_item_for_response($item, $request)
-    {
-        return $item;
-    }
+		$items          = array();
+		$items['users'] = $this->get_users( $date_args );
+		$items['posts'] = $this->get_posts( $date_args );
+		$items['pages'] = $this->get_posts( $date_args, 'page' );
+		$items['comments'] = $this->get_comments( $date_args );
 
-    /**
-     * Check if a given request has access to get items
-     *
-     * @param WP_REST_Request $request Full data about the request.
-     * @return WP_Error|bool
-     */
-    public function get_items_permissions_check($request)
-    {
-        return current_user_can('list_users');
-    }
+		/**
+		 * [$data description]
+		 * @var array
+		 */
+		$data = array();
+		foreach ( $items as $key => $item ) {
+			$itemdata     = $this->prepare_item_for_response( $item, $request );
+			$data[ $key ] = $this->prepare_response_for_collection( $itemdata );
+		}
 
-    /**
-     * Retrive weekly posts
-     *
-     * @since 1.0.0
-     */
-    public function get_weekly_posts($type = 'post')
-    {
-        $args = array(
-            'post_type' => $type,
-            'post_status' => 'publish',
-            'date_query' => array(
-                array(
-                    'after' => '1 week ago'
-                )
-            )
-        );
+		/**
+		 * [return description]
+		 * @var [type]
+		 */
+		return rest_ensure_response( $data );
+	}
 
-        $posts = get_posts($args);
-        return count($posts);
-    }
+	/**
+	 * Prepare the item for the REST response
+	 *
+	 * @param mixed $item WordPress representation of the item.
+	 * @param WP_REST_Request $request Request object.
+	 * @return mixed
+	 */
+	public function prepare_item_for_response( $item, $request ) {
+		return $item;
+	}
 
-    /**
-     * Retrive weekly comments
-     *
-     * @since 1.0.0
-     */
-    public function get_weekly_comments()
-    {
-        $args = array(
-            'date_query' => array(
-                array(
-                    'after' => '1 week ago'
-                )
-            )
-        );
+	/**
+	 * Check if a given request has access to get items
+	 *
+	 * @param WP_REST_Request $request Full data about the request.
+	 * @return WP_Error|bool
+	 */
+	public function get_items_permissions_check( $request ) {
+		return current_user_can( 'list_users' );
+	}
 
-        $comments = get_comments($args);
-        return count($comments);
-    }
+	/**
+	 * [get_users description]
+	 * @return [type] [description]
+	 */
+	public function get_users( $date_args ) {
+		/**
+		 * [global description]
+		 * @var [type]
+		 */
+		global $wpdb;
 
-    /**
-     * Retrive weekly users
-     *
-     * @since 1.0.0
-     */
-    public function get_weekly_users()
-    {
-        $args = array(
-            'date_query' => array(
-                array(
-                    'after' => '1 week ago'
-                )
-            )
-        );
+		/**
+		 * [$date_query description]
+		 * @var WP_Date_Query
+		 */
+		$date_query = new WP_Date_Query( $date_args );
 
-        $users = get_users($args);
-        return count($users);
-    }
+		/**
+		 * [$query_fields description]
+		 * @var string
+		 */
+		$query_fields  = "COUNT(*), EXTRACT(DAY FROM user_registered) day, EXTRACT(MONTH FROM user_registered) month, EXTRACT(YEAR FROM user_registered) year";
+		$query_from    = "FROM $wpdb->users";
+		$query_where   = "WHERE 1=1";
+		$query_where  .= $date_query->get_sql();
+		$query_groupby = "GROUP BY day, month, year";
+		$query_orderby = "ORDER BY user_registered ASC";
+
+		/**
+		 * [$request description]
+		 * @var string
+		 */
+		$request = "SELECT $query_fields $query_from $query_where $query_groupby $query_orderby";
+		$col     = $wpdb->get_col( $request );
+
+		/**
+		 * [$data description]
+		 * @var array
+		 */
+		$data = [
+			'total' => array_sum( $col ),
+			'data'  => $col,
+		];
+
+		/**
+		 * [return description]
+		 * @var [type]
+		 */
+		return $data;
+	}
+
+	/**
+	 * [get_posts description]
+	 * @return [type] [description]
+	 */
+	public function get_posts( $date_args, $post_type = 'post' ) {
+		/**
+		 * [global description]
+		 * @var [type]
+		 */
+		global $wpdb;
+
+		/**
+		 * [$date_query description]
+		 * @var WP_Date_Query
+		 */
+		$date_query = new WP_Date_Query( $date_args );
+
+		/**
+		 * [$query_fields description]
+		 * @var string
+		 */
+		$query_fields  = "COUNT(*), EXTRACT(DAY FROM post_date) day, EXTRACT(MONTH FROM post_date) month, EXTRACT(YEAR FROM post_date) year";
+		$query_from    = "FROM $wpdb->posts";
+		$query_where   = "WHERE post_type = '$post_type'";
+		$query_where  .= $date_query->get_sql();
+		$query_groupby = "GROUP BY day, month, year";
+		$query_orderby = "ORDER BY post_date ASC";
+
+		/**
+		 * [$request description]
+		 * @var string
+		 */
+		$request = "SELECT $query_fields $query_from $query_where $query_groupby $query_orderby";
+		$col     = $wpdb->get_col( $request );
+
+		/**
+		 * [$data description]
+		 * @var array
+		 */
+		$data = [
+			'total' => array_sum( $col ),
+			'data'  => $col,
+		];
+
+		/**
+		 * [return description]
+		 * @var [type]
+		 */
+		return $data;
+	}
+
+	/**
+	 * [get_comments description]
+	 * @param  [type] $date_args [description]
+	 * @return [type]            [description]
+	 */
+	public function get_comments( $date_args ) {
+		/**
+		 * [global description]
+		 * @var [type]
+		 */
+		global $wpdb;
+
+		/**
+		 * [$date_query description]
+		 * @var WP_Date_Query
+		 */
+		$date_query = new WP_Date_Query( $date_args );
+
+		/**
+		 * [$query_fields description]
+		 * @var string
+		 */
+		$query_fields  = "COUNT(*), EXTRACT(DAY FROM comment_date) day, EXTRACT(MONTH FROM comment_date) month, EXTRACT(YEAR FROM comment_date) year";
+		$query_from    = "FROM $wpdb->comments";
+		$query_where   = "WHERE 1=1";
+		$query_where  .= $date_query->get_sql();
+		$query_groupby = "GROUP BY day, month, year";
+		$query_orderby = "ORDER BY comment_date ASC";
+
+		/**
+		 * [$request description]
+		 * @var string
+		 */
+		$request = "SELECT $query_fields $query_from $query_where $query_groupby $query_orderby";
+		$col     = $wpdb->get_col( $request );
+
+		/**
+		 * [$data description]
+		 * @var array
+		 */
+		$data = [
+			'total' => array_sum( $col ),
+			'data'  => $col,
+		];
+
+		/**
+		 * [return description]
+		 * @var [type]
+		 */
+		return $data;
+	}
 }
--- includes/rest-api/hooks-users.php
+++ includes/rest-api/hooks-users.php
@@ -1,32 +1,32 @@
 <?php
 function wh_user_collection_params( $query_params ) {
-    $query_params['after'] = array(
-        'description'        => __( 'Limit response to users registered after a given ISO8601 compliant date.' ),
-        'type'               => 'string',
-        'format'             => 'date-time',
-    );
+	$query_params['after'] = array(
+		'description'        => __( 'Limit response to users registered after a given ISO8601 compliant date.' ),
+		'type'               => 'string',
+		'format'             => 'date-time',
+	);
 
-    $query_params['before'] = array(
-        'description'        => __( 'Limit response to users registered before a given ISO8601 compliant date.' ),
-        'type'               => 'string',
-        'format'             => 'date-time',
-    );
+	$query_params['before'] = array(
+		'description'        => __( 'Limit response to users registered before a given ISO8601 compliant date.' ),
+		'type'               => 'string',
+		'format'             => 'date-time',
+	);
 
-    return $query_params;
+	return $query_params;
 }
 add_filter( 'rest_user_collection_params', 'wh_user_collection_params' );
 
 function wh_user_query( $prepared_args, $request ) {
-    $prepared_args['date_query'] = array();
+	$prepared_args['date_query'] = array();
 
-    if ( isset( $request['before'] ) ) {
-        $prepared_args['date_query'][0]['before'] = $request['before'];
-    }
+	if ( isset( $request['before'] ) ) {
+		$prepared_args['date_query'][0]['before'] = $request['before'];
+	}
 
-    if ( isset( $request['after'] ) ) {
-        $prepared_args['date_query'][0]['after'] = $request['after'];
-    }
+	if ( isset( $request['after'] ) ) {
+		$prepared_args['date_query'][0]['after'] = $request['after'];
+	}
 
-    return $prepared_args;
+	return $prepared_args;
 }
 add_filter( 'rest_user_query', 'wh_user_query', 10, 2 );
