a2ab1a197f26ff60f85246b195163537dbdca9e8
Mithun Biswas <bhoot.biswas@gmail.com>
2018-07-24 23:22:34 +0600

    Update get_items method

--- includes/rest-api/class-wpdrift-users-controller.php
+++ includes/rest-api/class-wpdrift-users-controller.php
@@ -39,10 +39,15 @@ class WPdrift_Users_Controller extends WP_REST_Controller {
 	 * @param WP_REST_Request $request Current request.
 	 */
 	public function get_items_permissions_check( $request ) {
+		/**
+		 * Dev only.
+		 * @var [type]
+		 */
+		return true;
+
 		if ( ! current_user_can( 'list_users' ) ) {
 			return new WP_Error( 'rest_forbidden', esc_html__( 'You cannot view the post resource.' ), array( 'status' => $this->authorization_status_code() ) );
 		}
-		return true;
 	}
 
 	/**
@@ -52,10 +57,76 @@ class WPdrift_Users_Controller extends WP_REST_Controller {
 	 */
 	public function get_items( $request ) {
 		/**
+		 * [global description]
+		 * @var [type]
+		 */
+		global $wpdb;
+
+		/**
+		 * [$data description]
+		 * @var array
+		 */
+		$data      = array();
+		$date_args = array();
+
+		/**
+		 * [if description]
+		 * @var [type]
+		 */
+		if ( isset( $request['after'] ) ) {
+			$date_args[0]['after'] = $request['after'];
+		}
+
+		/**
+		 * [if description]
+		 * @var [type]
+		 */
+		if ( isset( $request['before'] ) ) {
+			$date_args[0]['before'] = $request['before'];
+		}
+
+		/**
+		 * [$date_query description]
+		 * @var WP_Date_Query
+		 */
+		$date_query = new WP_Date_Query( $date_args, 'user_registered' );
+
+		/**
+		 * [$query_fields description]
+		 * @var string
+		 */
+		$query_fields  = "COUNT(*), user_registered, EXTRACT(DAY FROM user_registered) day, EXTRACT(MONTH FROM user_registered) month, EXTRACT(YEAR FROM user_registered) year";
+		$query_from    = "FROM $wpdb->users";
+		$query_where   = "WHERE 1=1";
+		$query_where  .= $date_query->get_sql();
+		$query_groupby = "GROUP BY day, month, year";
+		$query_orderby = "ORDER BY user_registered ASC";
+
+		/**
+		 * [$request description]
+		 * @var string
+		 */
+		$request             = "SELECT $query_fields $query_from $query_where $query_groupby $query_orderby";
+		$col                 = $wpdb->get_col( $request );
+		$col_user_registered = $wpdb->get_col( $request, 1 );
+
+		/**
 		 * [$data description]
 		 * @var array
 		 */
 		$data = array();
+
+		/**
+		 * [$response description]
+		 * @var [type]
+		 */
+		$response = [
+			'total'  => array_sum( $col ),
+			'data'   => $col,
+			'labels' => $col_user_registered,
+		];
+
+		$data[] = $this->prepare_response_for_collection( $response );
 
 		/**
 		 * Return all of our comment response data.
