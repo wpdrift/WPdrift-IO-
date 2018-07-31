b996f7021bf97d1888f036e2f89f589c34bb9ff9
Mithun Biswas <bhoot.biswas@gmail.com>
2018-07-31 14:00:54 +0600

    Remove deprecated class

--- includes/class-wpdrift-io.php
+++ includes/class-wpdrift-io.php
@@ -151,7 +151,6 @@ class WO_Server {
 		 * @var [type]
 		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wpdrift-date-query.php';
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wpdrift-io-hits.php';
 
 		$this->loader = new WPdrift_IO_Loader();
 
@@ -185,6 +184,7 @@ class WO_Server {
 
 		$plugin_public = new WPdrift_IO_Public( $this->get_plugin_name(), $this->get_version() );
 
+		$this->loader->add_action( 'init', $plugin_public, 'hits' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 
 	}
--- public/class-wpdrift-io-public.php
+++ public/class-wpdrift-io-public.php
@@ -11,6 +11,15 @@
  */
 
 /**
+ * [use description]
+ * @var [type]
+ */
+use Models\Hit;
+use DeviceDetector\DeviceDetector;
+use DeviceDetector\Parser\Device\DeviceParserAbstract;
+use League\Uri;
+
+/**
  * The public-facing functionality of the plugin.
  *
  * Defines the plugin name, version, and two examples hooks for how to
@@ -52,6 +61,84 @@ class WPdrift_IO_Public {
 		$this->plugin_name = $plugin_name;
 		$this->version     = $version;
 
+	}
+
+	/**
+	 * [hits description]
+	 * @return [type] [description]
+	 */
+	public function hits() {
+
+		/**
+		 * [$hit description]
+		 * @var Models
+		 */
+		$hit          = new Models\Hit();
+		$hit->user_id = get_current_user_id();
+		$hit->agent   = isset( $_SERVER['HTTP_USER_AGENT'] ) ? $_SERVER['HTTP_USER_AGENT'] : '';
+		$hit->host    = isset( $_SERVER['HTTP_HOST'] ) ? $_SERVER['HTTP_HOST'] : '';
+		$hit->uri     = isset( $_SERVER['REQUEST_URI'] ) ? $_SERVER['REQUEST_URI'] : '';
+		$hit->ip      = isset( $_SERVER['REMOTE_ADDR'] ) ? $_SERVER['REMOTE_ADDR'] : '';
+		$hit->referer = isset( $_SERVER['HTTP_REFERER'] ) ? $_SERVER['HTTP_REFERER'] : '';
+
+		/**
+		 * [$dd description]
+		 * @var DeviceDetector
+		 */
+		$dd = new DeviceDetector( $hit->agent );
+		$dd->parse();
+
+		/**
+		 * [$client description]
+		 * @var [type]
+		 */
+		$client      = $dd->getClient();
+		$os          = $dd->getOs();
+		$device_name = $dd->getDeviceName();
+
+		/**
+		 * [$hit->client_type description]
+		 * @var [type]
+		 */
+		$hit->client_type       = isset( $client['type'] ) ? $client['type'] : '';
+		$hit->client_name       = isset( $client['name'] ) ? $client['name'] : '';
+		$hit->client_short_name = isset( $client['short_name'] ) ? $client['short_name'] : '';
+		$hit->client_version    = isset( $client['version'] ) ? $client['version'] : '';
+		$hit->client_engine     = isset( $client['engine'] ) ? $client['engine'] : '';
+
+		/**
+		 * [$hit->os_name description]
+		 * @var [type]
+		 */
+		$hit->os_name       = isset( $os['name'] ) ? $os['name'] : '';
+		$hit->os_short_name = isset( $os['short_name'] ) ? $os['short_name'] : '';
+		$hit->os_version    = isset( $os['version'] ) ? $os['version'] : '';
+		$hit->os_platform   = isset( $os['platform'] ) ? $os['platform'] : '';
+
+		/**
+		 * [$hit->os_platform description]
+		 * @var [type]
+		 */
+		$hit->device_name = isset( $device_name ) ? $device_name : '';
+
+		/**
+		 * [if description]
+		 * @var [type]
+		 */
+		$hit->domain = home_url();
+		if ( isset( $_SERVER['HTTP_REFERER'] ) ) {
+			$uri = Uri\parse( $_SERVER['HTTP_REFERER'] );
+
+			if ( $uri['host'] ) {
+				$hit->domain = $uri['host'];
+			}
+		}
+
+		/**
+		 * [$hit->save description]
+		 * @var [type]
+		 */
+		$hit->save();
 	}
 
 	/**
