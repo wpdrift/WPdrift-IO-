0c5931720be4ea7d4ed332d9a1d31b7f7b2c5f84
Mithun Biswas <bhoot.biswas@gmail.com>
2018-08-02 12:35:21 +0600

    Update users controller

--- includes/rest-api/class-wpdrift-users-controller.php
+++ includes/rest-api/class-wpdrift-users-controller.php
@@ -7,7 +7,8 @@
  * @since 1.0.0
  */
 
-use SebastianBergmann\Timer\Timer;
+use Carbon\Carbon;
+// use SebastianBergmann\Timer\Timer;
 
 /**
  * [WPdrift_Users_Controller description]
@@ -31,7 +32,7 @@ class WPdrift_Users_Controller extends WP_REST_Controller {
 			array(
 				'methods'             => WP_REST_Server::READABLE,
 				'callback'            => array( $this, 'get_items' ),
-				// 'permission_callback' => array( $this, 'get_items_permissions_check' ),
+				'permission_callback' => array( $this, 'get_items_permissions_check' ),
 			),
 		) );
 	}
@@ -58,12 +59,13 @@ class WPdrift_Users_Controller extends WP_REST_Controller {
 	 * @param WP_REST_Request $request Current request.
 	 */
 	public function get_items( $request ) {
+		// return array();
 
 		/**
 		 * [Timer description]
 		 * @var [type]
 		 */
-		Timer::start();
+		// Timer::start();
 
 		/**
 		 * [global description]
@@ -75,8 +77,14 @@ class WPdrift_Users_Controller extends WP_REST_Controller {
 		 * [$data description]
 		 * @var array
 		 */
-		$data      = array();
+		$data = array();
+
+		/**
+		 * [$date_args description]
+		 * @var array
+		 */
 		$date_args = array();
+		$mode      = isset( $request['mode'] ) ? $request['mode'] : 'day';
 
 		/**
 		 * [if description]
@@ -104,11 +112,11 @@ class WPdrift_Users_Controller extends WP_REST_Controller {
 		 * [$query_fields description]
 		 * @var string
 		 */
-		$query_fields  = "COUNT(*), user_registered, HOUR(user_registered) hour, DAY(user_registered) day, WEEK(user_registered) week, MONTH(user_registered) month, YEAR(user_registered) year";
+		$query_fields  = "COUNT(*), user_registered, MINUTE(user_registered) minute, HOUR(user_registered) hour, DAY(user_registered) day, WEEK(user_registered) week, MONTH(user_registered) month, YEAR(user_registered) year";
 		$query_from    = "FROM $wpdb->users";
 		$query_where   = "WHERE 1=1";
 		$query_where  .= $date_query->get_sql();
-		$query_groupby = "GROUP BY day, month, year";
+		$query_groupby = $this->query_groupby( $mode );
 		$query_orderby = "ORDER BY user_registered ASC";
 
 		/**
@@ -118,12 +126,6 @@ class WPdrift_Users_Controller extends WP_REST_Controller {
 		$query               = "SELECT $query_fields $query_from $query_where $query_groupby $query_orderby";
 		$col                 = $wpdb->get_col( $query );
 		$col_user_registered = $wpdb->get_col( $query, 1 );
-
-		/**
-		 * [$time description]
-		 * @var [type]
-		 */
-		$time = Timer::stop();
 
 		/**
 		 * [$data description]
@@ -136,19 +138,133 @@ class WPdrift_Users_Controller extends WP_REST_Controller {
 		 * @var [type]
 		 */
 		$response = [
-			'total'  => array_sum( $col ),
-			'data'   => $col,
-			'labels' => $col_user_registered,
-			'time'   => Timer::secondsToTimeString( $time ),
+			// 'query'   => $query,
+			'total'   => array_sum( $col ),
+			'data'    => $col,
+			'labels'  => $this->prepare_labels( $col_user_registered, $mode ),
+			'filters' => $this->get_filters( $date_args, $mode ),
 		];
 
-		$data[] = $this->prepare_response_for_collection( $response );
+		/**
+		 * [$data description]
+		 * @var [type]
+		 */
+		$data['results'] = $this->prepare_response_for_collection( $response );
+
+		/**
+		 * [$time description]
+		 * @var [type]
+		 */
+		// $time         = Timer::stop();
+		// $data['time'] = Timer::secondsToTimeString( $time );
 
 		/**
 		 * Return all of our comment response data.
 		 * @var [type]
 		 */
 		return rest_ensure_response( $data );
+	}
+
+	/**
+	 * [prepare_labels description]
+	 * @param  [type] $labels [description]
+	 * @return [type]         [description]
+	 */
+	public function prepare_labels( $dates, $mode ) {
+		/**
+		 * [$labels description]
+		 * @var array
+		 */
+		$labels = array();
+
+		foreach ( $dates as $date ) {
+			// $labels[] = $date;
+			$dt       = Carbon::createFromFormat( 'Y-m-d H:i:s', $date );
+			$labels[] = $this->formated_date( $dt, $mode );
+		}
+
+		/**
+		 * [return description]
+		 * @var [type]
+		 */
+		return $labels;
+	}
+
+	/**
+	 * [query_groupby description]
+	 * @return [type] [description]
+	 */
+	public function query_groupby( $mode ) {
+		/**
+		 * [switch description]
+		 * @var [type]
+		 */
+		switch ( $mode ) {
+			case 'minute':
+				return "GROUP BY minute, hour, day, month, year";
+				break;
+			case 'hour':
+				return "GROUP BY hour, day, month, year";
+				break;
+			case 'week':
+				return "GROUP BY week, month, year";
+				break;
+			case 'month':
+				return "GROUP BY month, year";
+				break;
+			case 'year':
+				return "GROUP BY year";
+				break;
+			default:
+				return "GROUP BY day, month, year";
+		}
+	}
+
+	/**
+	 * [formated_date description]
+	 * @param  [type] $dt   [description]
+	 * @param  [type] $mode [description]
+	 * @return [type]       [description]
+	 */
+	public function formated_date( $dt, $mode ) {
+		switch ( $mode ) {
+			case 'minute':
+				return $dt->format( 'h:i A' );
+				break;
+			case 'hour':
+				return $dt->format( 'h A, d' );
+				break;
+			case 'week':
+				return $dt->format( 'd M y' );
+				break;
+			case 'month':
+				return $dt->format( 'M Y' );
+				break;
+			case 'year':
+				return $dt->format( 'Y' );
+				break;
+			default:
+				return $dt->toFormattedDateString();
+		}
+	}
+
+	/**
+	 * [get_filters description]
+	 * @param  [type] $dates [description]
+	 * @param  [type] $mode  [description]
+	 * @return [type]        [description]
+	 */
+	public function get_filters( $dates, $mode ) {
+		$filters = [
+			'minute' => __( 'Minute', 'wpdrift-io' ),
+			'hour'   => __( 'hour', 'wpdrift-io' ),
+			'day'    => __( 'Day', 'wpdrift-io' ),
+			'week'   => __( 'Week', 'wpdrift-io' ),
+			'month'  => __( 'Month', 'wpdrift-io' ),
+			'year'   => __( 'Year', 'wpdrift-io' ),
+		];
+
+		return $filters;
 	}
 
 	/**
