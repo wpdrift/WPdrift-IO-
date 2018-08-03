34743daf0d1f89824947a3f6b768c17f72529180
Mithun Biswas <bhoot.biswas@gmail.com>
2018-08-03 13:32:09 +0600

    Remove deprecated function wo_admin_setting_tabs

--- includes/functions.php
+++ includes/functions.php
@@ -502,45 +502,6 @@ function wo_is_protocol_secure()
 	return $isSecure;
 }
 
-/**
- * Setup the admin tabs as needed
- *
- * @param $page
- * @param $tabs
- * @param $location
- * @param $default
- * @param null $current
- */
-function wo_admin_setting_tabs($page, $tabs, $location, $default, $current = null)
-{
-	if (is_null($current)) {
-		if (isset($_GET['tab'])) {
-			$current = $_GET['tab'];
-		} else {
-			$current = $default;
-		}
-	}
-	$content = '';
-	$content .= '<h2 class="nav-tab-wrapper">';
-	foreach ($tabs as $tab => $tabname) {
-		if ($current == $tab) {
-			$class = ' nav-tab-active';
-		} else {
-			$class = '';
-		}
-		$content .= '<a class="nav-tab' . $class . '" href="?page=' .
-					$page . '&tab=' . $tab . '">' . $tabname . '</a>';
-	}
-	$content .= '</h2>';
-	echo $content;
-	if (! $current) {
-		$current = key($tabs);
-	}
-	require_once($location . $current . '.php');
-
-	return;
-}
-
 // Public Functions.
 require_once(dirname(__FILE__) . '/public.php');
 
