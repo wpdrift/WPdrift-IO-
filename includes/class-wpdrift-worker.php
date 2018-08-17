5d73d82335bc8f0d4a0a667c4af35fae00ae1f1a
Mithun Biswas <bhoot.biswas@gmail.com>
2018-08-17 13:38:30 +0600

    Move functions

--- includes/class-wpdrift-worker.php
+++ includes/class-wpdrift-worker.php
@@ -128,7 +128,6 @@ class WPdrift_Worker {
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . '/includes/actions.php';
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . '/includes/post-types.php';
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . '/includes/functions.php';
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . '/includes/public.php';
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . '/includes/rest-api/hooks.php';
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . '/wpdrift-worker-deprecated.php';
 
--- includes/functions.php
+++ includes/functions.php
@@ -311,3 +311,112 @@ function wpdrift_worker_is_dev()
 {
 	return _WPDW()->env == 'development' ? true : false;
 }
+
+/**
+ * Public Functions for WP OAuth Server
+ *
+ * @author Justin Greer  <jusin@justin-greer.com>
+ * @package WP OAuth Server
+ */
+
+/**
+ *
+ * @deprecated in favor of wpdrift_worker_public_get_access_token
+ */
+function wpdrift_worker_get_access_token( $access_token, $return_type = ARRAY_A ) {
+
+	$data = wpdrift_worker_public_get_access_token( $access_token, $return_type );
+
+	return $data;
+}
+
+/**
+ * Retrieve information about an access token
+ *
+ * @param $access_token
+ * @param string $return_type
+ *
+ * @return array|bool|null|object|void
+ */
+function wpdrift_worker_public_get_access_token( $access_token, $return_type = ARRAY_A ) {
+	if ( is_null( $access_token ) ) {
+		return false;
+	}
+
+	global $wpdb;
+	$prepare_query = $wpdb->prepare(
+		"
+		SELECT *
+		FROM {$wpdb->prefix}oauth_access_tokens
+		WHERE access_token = %s
+		LIMIT 1
+		",
+		array( $access_token )
+	);
+
+	$access_token = $wpdb->get_row( $prepare_query, $return_type );
+	if ( $access_token ) {
+		$expires = strtotime( $access_token['expires'] );
+		if ( current_time( 'timestamp' ) > $expires ) {
+			return false;
+		}
+	}
+
+	return $access_token;
+}
+
+/**
+ * Insert a new OAuth 2 client
+ *
+ * @param null $client_data
+ *
+ * @return bool|int|WP_Error
+ */
+function wpdrift_worker_public_insert_client( $client_data = null ) {
+
+	do_action( 'wpdrift_worker_before_create_client', array( $client_data ) );
+
+	$client_id     = wpdrift_worker_gen_key();
+	$client_secret = wpdrift_worker_gen_key();
+
+	$grant_types = isset( $client_data['grant_types'] ) ? $client_data['grant_types'] : array();
+	$user_id     = isset( $client_data['user_id'] ) ? intval( $client_data['user_id'] ) : 0;
+
+	$client = array(
+		'post_title'     => wp_strip_all_tags( $client_data['name'] ),
+		'post_status'    => 'publish',
+		'post_author'    => 1,
+		'post_type'      => 'oauth_client',
+		'comment_status' => 'closed',
+		'meta_input'     => array(
+			'client_id'     => $client_id,
+			'client_secret' => $client_secret,
+			'grant_types'   => $grant_types,
+			'redirect_uri'  => sanitize_text_field( $client_data['redirect_uri'] ),
+			'user_id'       => $user_id,
+			'scope'         => sanitize_text_field( $client_data['scope'] ),
+		),
+	);
+
+	// Insert the post into the database
+	$client_insert = wp_insert_post( $client );
+	if ( is_wp_error( $client_insert ) ) {
+		return $client_insert->get_error_message();
+	}
+
+	return $client_insert;
+}
+
+// print_r(wpdrift_worker_public_insert_client( array(
+// 	'name'         => __( 'Example', 'wpdrift-worker' ),
+// 	'user_id'      => 1,
+// 	'grant_types'  => array(
+// 		'authorization_code',
+// 		'implicit',
+// 		'password',
+// 		'client_credentials',
+// 		'refresh_token',
+// 	),
+// 	'scope'        => 'basic',
+// 	'redirect_uri' => 'http://localhost/php/files/cb.php',
+// )));
