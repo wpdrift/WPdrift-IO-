b6b1d62bc7ac4c7028f90936baa2b1716b5d6a69
Mithun Biswas <bhoot.biswas@gmail.com>
2018-10-30 14:32:13 +0600

    Remove unnecessary functions

--- wpdrift-worker.php
+++ wpdrift-worker.php
@@ -74,21 +74,7 @@ function run_wpdrift_worker() {
 	_wpdw()->run();
 }
 
-// Debugging part
-if (!function_exists('_custlog')) {
-    function _custlog($message)
-    {
-        if (WP_DEBUG === true) {
-            if (is_array($message) || is_object($message)) {
-                error_log('<<<<<<<< :: DEBUG Array :: >>>>>>>>');
-                error_log(print_r($message, true));
-            } else {
-                error_log('<<<<<<<< :: DEBUG String :: >>>>>>>>');
-                error_log($message);
-            }
-        }
-    }
-}
+run_wpdrift_worker();
 
 /**
  * Detect EDD plugin. Then add edd webhooks
@@ -101,4 +87,3 @@ if ( in_array( 'easy-digital-downloads/easy-digital-downloads.php', (array) get_
 	 */
 	require_once( dirname( WPDRIFT_WORKER_FILE ) . '/includes/rest-api/edd/class-edd-webhooks.php' );
 }
-run_wpdrift_worker();
